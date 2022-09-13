68fce17a17e0b093cfd1b3fd3278cd27babb628f
SamSotsky <sam.sotsky01@gmail.com>
2022-09-13 13:50:32 -0700

    LeetCode

--- TwoSum.js
+++ TwoSum.js
@@ -1,2 +1,18 @@
 nums = [3,2,4]
-var twoSum = function()
+var twoSum = function(nums, target) {
+    for(i=0;i<nums.length-1;i++){
+        temp1=nums[i]
+        for(j=nums.length-1;j>i;j--){
+            temp2+nums[j]
+            if(j==i){
+                continue
+            }
+            if(temp1+temp2==target){
+                return [i+j]
+            }
+        }
+    }
+    return "can not add to target"
+};
+
+console.log(twoSum(nums,6))
